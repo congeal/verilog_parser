241ce1e5b434c9ba6cb4066c690963c65c888f93
Eung Sup Kim <congeal@me.com>
2018-07-22 03:02:43 +0900

    Conditional operator 추가

--- verilog_parser.py
+++ verilog_parser.py
@@ -8,6 +8,9 @@ comment */ 444;
 assign a_wire = (3+4)*2 + 4 - 1;
 assign b_wire = a_wire << 2;
 assign c_wire = -a**2;
+assign d_wire = a_wire < b_wire;
+assign e_wire = a_wire !== b_wire;
+assign f_wire = a || b ? (b ? 1 : 0) : (c ? 0 : 1);
 endmodule"""
 
 with open('verilog.lark') as f:
