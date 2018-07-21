f5b82de3d82bb2f96326721dff4fe76c61864baf
Eung Sup Kim <congeal@me.com>
2018-07-22 03:19:45 +0900

    always construct rule 추가 (event/timing control 빼고)

--- verilog_parser.py
+++ verilog_parser.py
@@ -3,14 +3,18 @@ from lark import Lark, Transformer, v_args
 src = """module pipe_pal
 #(param, /*some comment*/ localparam)
 (a, b, c); /* multi-line
-comment */ 444;
-333; //expression
-assign a_wire = (3+4)*2 + 4 - 1;
-assign b_wire = a_wire << 2;
+comment */ assign a_wire = (3+4)*2 + 4 - 1;
+assign b_wire = a_wire << 2; //expression
 assign c_wire = -a**2;
 assign d_wire = a_wire < b_wire;
 assign e_wire = a_wire !== b_wire;
 assign f_wire = a || b ? (b ? 1 : 0) : (c ? 0 : 1);
+
+always
+    c = a + b;
+
+always
+    q_c <= q_c + 1;
 endmodule"""
 
 with open('verilog.lark') as f:
